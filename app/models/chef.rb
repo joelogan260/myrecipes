5ce5d2273a1b8ace90b8a62b0fc123a810f93aca
Joel Aguilar <joel.aguilarg@gmail.com>
2019-09-30 16:42:24 -0500

    Add edit action with tests

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -8,6 +8,6 @@ class Chef < ApplicationRecord
 
 	has_many :recipes
 	has_secure_password
-	validates :password, presence: true, length: {minimum: 8}
+	validates :password, presence: true, length: {minimum: 8}, allow_nil: true
 
 end
